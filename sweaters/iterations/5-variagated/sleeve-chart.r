db06318d4bdefc709e0210ae4b9257fde5a156c2
Nate Wessel <nate@natewessel.com>
2026-04-04 19:04:27 -0400

    continue delevoping sleeve calculator

--- sweaters/iterations/5-variagated/sleeve-chart.r
+++ sweaters/iterations/5-variagated/sleeve-chart.r
@@ -6,20 +6,22 @@ library('ggplot2')
 cm_per_100_rows = 28.5
 cm_per_100_st = 39.0
 
-stAtUnderarm = 106
+stAtUnderarm = 135
 
-sleeveCmFromUnderarm = 45 # cm
+sleeveCmFromUnderarm = 55 # cm
+
+cuffRows = 20 # no decreases here
 
 stFromUnderarm = ceiling(
   sleeveLengthFromUnderarm / (cm_per_100_rows / 100)
 )
 # sleeve row counts where the decrease rate changes
 pivotPoints = tibble(
-  rowStart = c(1, 10, 60),
-  decreaseRate =  c(1, 1/2, 1/4)
+  rowStart =      c(1, 16,  55,  100, stFromUnderarm-cuffRows),
+  decreaseRate =  c(1, 1/2, 1/3, 1/4, 0)
 )
 
-tibble(row = seq(1, stFromUnderarm)) %>%
+rows = tibble(row = seq(1, stFromUnderarm)) %>%
   inner_join(
     pivotPoints,
     join_by(closest(row >= rowStart))
@@ -36,12 +38,29 @@ tibble(row = seq(1, stFromUnderarm)) %>%
     stLeft = stAtUnderarm/2 - decreasesLeft,
     # right from centreline
     stRight = stAtUnderarm/2 - decreasesRight
-  ) %>%
+  )
+
+rows %>%
   ggplot( aes( x=row ) ) +
   geom_step(aes(y=-stRight)) +
   geom_step(aes(y=stLeft)) +
+  geom_vline(data=pivotPoints, aes(xintercept=rowStart), colour='pink' ) +
+  geom_vline( aes(xintercept=33), colour='orange') +
   xlab('Rows') +
   ylab('Stitches') +
   coord_fixed( ratio = cm_per_100_st / cm_per_100_rows ) +
-  scale_x_continuous(minor_breaks = seq(0,stFromUnderarm,2)) +
-  scale_y_continuous(minor_breaks = seq(-80,80,2))
+  scale_x_continuous(
+    breaks = seq(0,stFromUnderarm,10),
+    minor_breaks = seq(0,stFromUnderarm,2)
+  ) +
+  scale_y_continuous(
+    breaks = seq(-80,80,10),
+    minor_breaks = seq(-80,80,2)
+  ) +
+  labs(title='Sweeater sleeve decrease guide')
+
+rows %>%
+  summarize(
+    st_at_wrist = min(st),
+    cm_at_wrist = min(st) * cm_per_100_st / 100
+  )
